fd1b8bf51b101352fc1dbbef1fd56b6aedb589c8
engie.matene <engie.matene@trademe.co.nz>
2021-02-25 09:47:56 +1300

    basic hello world console app

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Kia ora Te Ao Marama")
+
+	// In case of console app, below allows user input
+	//reader := bufio.NewReader(os.Stdin)
+	//text, _, _:= reader.ReadLine()
+
+	return
+}
+
+
